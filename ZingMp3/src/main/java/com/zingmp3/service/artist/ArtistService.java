9e15f5260ebf3f213d2d755cad69651b26603b5f
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-08-05 15:08:19 +0700

    thao

--- ZingMp3/src/main/java/com/zingmp3/service/artist/ArtistService.java
+++ ZingMp3/src/main/java/com/zingmp3/service/artist/ArtistService.java
@@ -1,2 +1,38 @@
-package com.zingmp3.service.artist;public class ArtistService {
+package com.zingmp3.service.artist;
+
+import com.zingmp3.model.Artist;
+import com.zingmp3.repository.IArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ArtistService implements IArtistService {
+    @Autowired
+    private IArtistRepository artistRepository;
+    @Override
+    public Iterable<Artist> findAll() {
+        return artistRepository.findAll();
+    }
+
+    @Override
+    public Optional<Artist> findById(Long id) {
+        return artistRepository.findById(id);
+    }
+
+    @Override
+    public Artist save(Artist artist) {
+        return artistRepository.save(artist);
+    }
+
+    @Override
+    public void delete(Long id) {
+artistRepository.deleteById(id);
+    }
+
+    @Override
+    public Artist findByName(String name) {
+        return artistRepository.findByName(name);
+    }
 }
--- ZingMp3/src/main/java/com/zingmp3/service/artist/IArtistService.java
+++ ZingMp3/src/main/java/com/zingmp3/service/artist/IArtistService.java
@@ -1,2 +1,8 @@
-package com.zingmp3.service.artist;public interface IArtistService {
+package com.zingmp3.service.artist;
+
+import com.zingmp3.model.Artist;
+import com.zingmp3.service.IGeneralService;
+
+public interface IArtistService extends IGeneralService<Artist> {
+    Artist findByName(String name);
 }
